7db5b580e0a7d8ae6f522753b22bad710f7cab59
Bernardo Rodrigues <67072582+Sentessh@users.noreply.github.com>
2025-11-29 20:43:08 -0300

    Update main method to use JanelaPrincipal
    Replaced MenuPrincipal with JanelaPrincipal for UI.

--- progmod/src/agenciaespacial/Programa.java
+++ progmod/src/agenciaespacial/Programa.java
@@ -1,15 +1,13 @@
 package agenciaespacial;
 
-import visao.MenuPrincipal;
 import Persistencia.BancoDeDados;
+import visao.JanelaPrincipal;
 
 public class Programa {
 
-    public static void main(String[] args) {  
+    public static void main(String[] args) {
         BancoDeDados banco = new BancoDeDados();
-        MenuPrincipal menu = new MenuPrincipal(banco);
-        System.out.println("### SISTEMA DE GERENCIAMENTO DE MISSÕES ESPACIAIS ###");
-        menu.executar();
-        System.out.println("\nPrograma encerrado. Tenha um bom dia!");
+        JanelaPrincipal janela = new JanelaPrincipal();
+        janela.setVisible(true);
     }
 }
